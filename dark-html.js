ad10a55f86f7546cd2b4a5714aa8b8ef1161c24e
robbrotherton <robert.brotherton@gmail.com>
2022-10-31 15:13:02 -0400

    add dark mode toggle on main html pages

---
+++ dark-html.js
@@ -0,0 +1,23 @@
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+
+<script>
+var toggle = $('a[href$="_"]')
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+toggle.click(function( event ) {
+  event.preventDefault();
+
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+});
+</script>
